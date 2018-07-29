8513d0b4eebfa17ff006f76804e167b2d7056930
gashaaa <gasha@DESKTOP-TN916LB>
2018-07-29 06:48:02 +0300

    add eventtimeline angular component

---
+++ MeetingTrackManagement/ClientApp/app/components/EventTimeline/eventtimeline.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-eventtimeline',
+    templateUrl: './eventtimeline.component.html',
+    styleUrls: ['./eventtimeline.component.css']
+})
+export class EventTimelineComponent {
+}
