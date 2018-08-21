ce6506a8a83c6b1078203df204f8c778a431a7e0
gashaaa <gasha@DESKTOP-TN916LB>
2018-08-21 14:21:39 +0300

    added static class to shuffle list

---
+++ MeetingTrackManagement/Service/ShuffleList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingTrackManagement.Service
+{
+    public static class ShuffleList
+    {
+        public static List<E> Shuffle<E>(List<E> listToShuffle)
+        {
+            List<E> randomList = new List<E>();
+
+            Random r = new Random();
+            int randomIndex = 0;
+            while (listToShuffle.Count > 0)
+            {
+                randomIndex = r.Next(0, listToShuffle.Count);
+                randomList.Add(listToShuffle[randomIndex]);
+                listToShuffle.RemoveAt(randomIndex);
+            }
+
+            return randomList;
+
+        }
+    }
+}
